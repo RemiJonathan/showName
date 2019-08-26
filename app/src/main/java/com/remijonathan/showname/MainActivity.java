97dd812717719e848052262247c184a57b194a59
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-08-26 14:51:01 -0400

    Created code to show text

--- app/src/main/java/com/remijonathan/showname/MainActivity.java
+++ app/src/main/java/com/remijonathan/showname/MainActivity.java
@@ -3,12 +3,33 @@ package com.remijonathan.showname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showMe;
+    private TextView showText;
+    private EditText userText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showMe = findViewById(R.id.button_show_name);
+        showText = findViewById(R.id.textView);
+        userText = findViewById(R.id.editText);
+
+        showMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = userText.getText().toString();
+                showText.setText("Hello, "+name+", how are you?");
+            }
+        });
     }
 }
