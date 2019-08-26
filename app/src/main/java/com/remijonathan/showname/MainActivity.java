a6697b8effb31762bc2cea3a43b1383b4650859b
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-08-26 15:09:40 -0400

    Created Toast

--- app/src/main/java/com/remijonathan/showname/MainActivity.java
+++ app/src/main/java/com/remijonathan/showname/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button showMe;
     private TextView showText;
     private EditText userText;
+    private Button showTag;
 
 
 
@@ -23,12 +25,20 @@ public class MainActivity extends AppCompatActivity {
         showMe = findViewById(R.id.button_show_name);
         showText = findViewById(R.id.textView);
         userText = findViewById(R.id.editText);
+        showTag = findViewById(R.id.button_show_tag);
 
         showMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String name = userText.getText().toString();
                 showText.setText("Hello, "+name+", how are you?");
+            }
+        });
+
+        showTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hi! I'm a Nice, Hot, Toast",Toast.LENGTH_LONG).show();
             }
         });
     }
